bf2ff1b9d0e44b73d5008cd9dd01e974263951e1
Logan Zartman <logan.zartman@utexas.edu>
2017-09-26 01:07:13 -0500

    Flask echo route demo

--- main.py
+++ main.py
@@ -2,16 +2,21 @@
 import logging
 
 from flask import Flask
-
+from flask import jsonify
 
 app = Flask(__name__)
 
 @app.route('/api/hello')
-def hello():
+def show_hello():
     return 'Poupon? :)'
 
+# "RESTful" JSON API
+@app.route('/api/echo/<string:what>')
+def show_echo(what):
+    return jsonify({'text': what})
+
 @app.route('/api/cities')
-def cities():
+def show_cities():
     return 'dsfsdfThis is where we display cities!!!'
 
 @app.errorhandler(500)
