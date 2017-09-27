6777fcd180f937bea7ae6561e172969fc3d4efc4
Logan Zartman <logan.zartman@utexas.edu>
2017-09-26 21:45:09 -0500

    Added bootstrap to react site

--- poupon-web/src/index.js
+++ poupon-web/src/index.js
@@ -1,5 +1,11 @@
+//React imports
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+//Bootstrap imports
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
