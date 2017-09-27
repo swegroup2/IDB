a7ebc51baa8d2a96935cae5fe5611aed624eea55
Logan Zartman <logan.zartman@utexas.edu>
2017-09-26 21:57:13 -0500

    Reverted adding react-bootstrap in favor of plain bootstrap

--- poupon-web/src/App.js
+++ poupon-web/src/App.js
@@ -1,36 +1,18 @@
 import React, { Component } from 'react';
-import { Navbar, Jumbotron, Button, Grid } from 'react-bootstrap';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Navbar inverse fixedTop>
-          <Grid>
-            <Navbar.Header>
-              <Navbar.Brand>
-                <a href="/">Poupon.me</a>
-              </Navbar.Brand>
-              <Navbar.Toggle />
-            </Navbar.Header>
-          </Grid>
-        </Navbar>
-        <Jumbotron>
-          <Grid>
-            <h1>Welcome to Poupon</h1>
-            <p>
-              <Button
-                bsStyle="success"
-                bsSize="large"
-                href="http://poupon.me/api/hello"
-                target="_blank">
-                API Test
-              </Button>
-            </p>
-          </Grid>
-        </Jumbotron>
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
