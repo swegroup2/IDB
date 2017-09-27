84dd96549839064b97d6dec92349c151c42cba85
Logan Zartman <logan.zartman@utexas.edu>
2017-09-26 22:14:36 -0500

    Readded bootstrap

--- poupon-web/src/App.js
+++ poupon-web/src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+      <h1>Welcome to Poupon!</h1>
+      <p>Rendered with React.</p>
+      <a href="http://poupon.me/api/hello" target="_blank">
+        <button type="button" className="btn btn-primary">API Test</button>
+      </a>
       </div>
     );
   }
