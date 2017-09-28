a5a35170e5a6c7928ca815e0da8d278d9730c5dc
sarah <swang622@gmail.com>
2017-09-28 16:02:21 -0500

    individual news cards

--- poupon-web/src/News.js
+++ poupon-web/src/News.js
@@ -1,23 +1,57 @@
 import React, { Component } from 'react';
+import {
+  Route,
+  Switch
+} from 'react-router-dom';
 
-var data = require('./data.json').data[2];
+var data = require('./data.json').data;
 
 class News extends Component {
     render() {
-        const items = data.news.map(item => <Preview news={item}/>);
         return (
-            <div className="Container">
+            <Switch>
+                <Route exact path="/news" component={MultipleNews}/>
+                <Route component={SingleNews}/>
+            </Switch>
+        );
+    }
+}
+
+class SingleNews extends Component {
+    render() {
+        var path = window.location.pathname.split("/");
+        var id = path[2];
+        console.log("id: " + id);
+        for(var i = 0; i < data.news.length; i++) {
+            if(data.news[i].id == id) {
+                return (
+                        <div className="row">
+                        <div className="col-sm-0 col-md-3">
+                        </div>
+                        <NewsCard news={data.news[i]}/>
+                        <div className="col-sm-0 col-md-3">
+                        </div>
+                        </div>
+                       );
+            }
+        }
+    }
+}
+
+class MultipleNews extends Component {
+    render() {
+        const items = data.news.map((item, i) => <NewsCard key={i} news={item}/>);
+        return (
             <div className="row">
                 {items}
-            </div>
             </div>
      	);
 	}
 }
 
-class Preview extends Component {
+class NewsCard extends Component {
     render() {
-    	const {title, score, num_comments, url, artists} = this.props.news;
+    	const {id, title, score, num_comments, url, artists} = this.props.news;
     	const dStart = url.indexOf("//") + 2;
     	const dEnd = url.indexOf("/", dStart);
     	const domain = url.substring(dStart, dEnd);
@@ -27,7 +61,7 @@ class Preview extends Component {
                 <div className="card">
                   <div className="Container">
 				  <div className="card-block">
-				    <h4 className="card-title">{title}</h4>
+				    <h4 className="card-title"><a href={"news/"+id}>{title}</a></h4>
 				    <h6 className="card-subtitle mb-2 text-muted">{"points: " + score}</h6>
 				    <p className="card-text">{"Artists: " + artists.join(", ")}</p>
 				    <a href={url} className="card-link">{`Open (${domain})`}</a>
