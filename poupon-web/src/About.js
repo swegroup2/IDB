76307ed2aa10405a0fc97d542a8147de670eef7e
sarah <swang622@gmail.com>
2017-09-28 20:48:17 -0500

    about page summary card

--- poupon-web/src/About.js
+++ poupon-web/src/About.js
@@ -12,8 +12,40 @@ class About extends Component {
         return (
             <div className="Container">
             <div className="row">
+                <AboutSummary/>
                 {cards}
             </div>
+            </div>
+        );
+    }
+}
+
+class AboutSummary extends Component {
+    render() {
+        return (
+            <div className="col-lg-4 col-md-6 col-sm-12">
+                <div className="card">
+                    <div className="card-body">
+                        <h2>Poupon</h2>
+                        <p className="card-text">
+                        Poupon is a site dedicated to the discovery of hip-hop and r&b. Its purpose is to keep users informed of current events in the industry, as well as its impact around America. The intended audience is anyone in the general public who is interested in hip-hop music and culture.
+                        </p>
+                        <table className="table table-sm">
+                        <tbody>
+                        <tr><td><a href="https://github.com/swegroup2/IDB">GitHub</a></td><td></td></tr>
+                        <tr><td><a href="https://trello.com/b/WkHJVGMT/idb1-board">Trello</a></td><td></td></tr>
+                        <tr><td><a href="http://docs.sarahgrace.apiary.io/#">Apiary</a></td><td></td></tr>
+                        <tr><td><a href="https://utexas.app.box.com/s/ifvpz6r63gsh6ib7fl01oj4wp1ytm87e">Report</a></td><td></td></tr>
+                        <tr><td>Total Commits</td><td>100</td></tr>
+                        <tr><td>Total Issues</td><td>33</td></tr>
+                        <tr><td>Total Unit Tests</td><td>0</td></tr>
+                        </tbody>
+                        </table>
+                        <b>Data Sources: </b><a href="https://developer.spotify.com/web-api/endpoint-reference/">Spotify API</a>, <a href="https://www.reddit.com/dev/api/">Reddit API</a>, <a href="https://docs.genius.com/#annotations-h2">Genius API</a>*<br/>
+                        <b>Tools: </b><a href="https://facebook.github.io/react/">React</a>, <a href="http://flask.pocoo.org/">Flask</a>, <a href="http://getbootstrap.com/">Bootstrap</a>, <a href="https://github.com/">GitHub</a>, <a href="https://github.com/facebookincubator/create-react-app">Create-React-App</a>*<br/>
+                        *more details in report
+                    </div>
+                </div>
             </div>
         );
     }
