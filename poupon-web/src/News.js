7e430fe0ad60e0f9e226556ba9b00869e2b3a856
sarah <swang622@gmail.com>
2017-09-28 19:24:15 -0500

    added hyperlinks to news pages linking to other models

--- poupon-web/src/News.js
+++ poupon-web/src/News.js
@@ -5,6 +5,7 @@ import {
 } from 'react-router-dom';
 
 var data = require('./data.json').data;
+var db = require('./database.json').database;
 
 class News extends Component {
     render() {
@@ -51,25 +52,38 @@ class MultipleNews extends Component {
 
 class NewsCard extends Component {
     render() {
-    	const {id, title, score, num_comments, url, artists} = this.props.news;
+    	const {id, title, score, num_comments, url, artists, albums} = this.props.news;
     	const dStart = url.indexOf("//") + 2;
     	const dEnd = url.indexOf("/", dStart);
     	const domain = url.substring(dStart, dEnd);
+        const related_artists = artists.map(item =>
+            {if(item in db) {
+                return(<span><a href={"/artists/"+db[item]}>{item}</a>, </span>);
+            } else {
+                return(<span>{item}, </span>);
+            }}
+            );
+        const related_albums = albums.map(item =>
+            {if(item in db) {
+                return(<span><a href={"/albums/"+db[item]}>{item}</a>, </span>);
+            } else {
+                return(<span>{item}, </span>);
+            }}
+            );
         return (
-            <div className="col-sm-12 col-md-6">
-            	<div className="Container">
+                <div className="col-sm-12 col-md-6">
                 <div className="card">
-                  <div className="Container">
-				  <div className="card-block">
-				    <h4 className="card-title"><a href={"/news/"+id}>{title}</a></h4>
-				    <h6 className="card-subtitle mb-2 text-muted">{"points: " + score}</h6>
-				    <p className="card-text">{"Artists: " + artists.join(", ")}</p>
-				    <a href={url} className="card-link">{`Open (${domain})`}</a>
-				  </div>
-				  </div>
-				</div>
-            	</div>
-            </div>
+                <div className="card-body">
+                <h4 className="card-title"><a href={"/news/"+id}>{title}</a></h4>
+                <h6 className="card-subtitle mb-2 text-muted">{"points: " + score}</h6>
+                <p className="card-text">
+                <b>Related Artists: </b>{related_artists}<br/>
+                <b>Related Albums: </b>{related_albums}
+                </p>
+                <a href={url} className="card-link">{`Open (${domain})`}</a>
+                </div>
+                </div>
+                </div>
         );
     }
 }
