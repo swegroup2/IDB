ed7afd14dc57c721e99101bf1c008a5951b5897f
Logan Zartman <logan.zartman@utexas.edu>
2017-10-23 12:52:39 -0500

    Fixed database URL, fixed artists endpoint prototype

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import logging
 import os
 
 from flask import Flask
-from flask import jsonify
 from flask_sqlalchemy import SQLAlchemy
-from api.schema import *
+from database.schema import *
+from database.util import *
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
@@ -15,21 +15,19 @@ db = SQLAlchemy(app)
 
 @app.route('/api/hello')
 def show_hello():
-    return 'Poupon? :)'
+    return jsonify({'hello': 'world'})
 
 # "RESTful" JSON API
 @app.route('/api/echo/<string:what>')
 def show_echo(what):
     return jsonify({'text': what})
 
+@app.route('/api/artists/top/<integer:num>')
 @app.route('/api/artists/top')
-def show_artists():
-    matches = db.session.query(Artist).order_by(Artist.popularity).limit(5).all()
-    return jsonify(matches)
-
-@app.route('/api/cities')
-def show_cities():
-    return 'dsfsdfThis is where we display cities!!!'
+def show_artists(num=5):
+	num = max(1, min(10, num))
+    matches = db.session.query(Artist).order_by(Artist.popularity.desc()).limit(num).all()
+    return sql_json(Artist, *matches)
 
 @app.errorhandler(500)
 def server_error(e):
