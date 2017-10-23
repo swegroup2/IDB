77b77c703fd916b7d8a304ab380f6e3ec26d1f57
Logan Zartman <logan.zartman@utexas.edu>
2017-10-22 20:38:20 -0500

    Updated test to create instances, added .erdplus file

--- API/test-schema.py
+++ API/test-schema.py
@@ -1,5 +1,22 @@
 from schema import *
 from sqlalchemy import create_engine
-engine = create_engine('sqlite:///:memory:', echo=True)
+from sqlalchemy.orm import sessionmaker
 
+# engine = create_engine('sqlite:///:memory:', echo=True)
+engine = create_engine('postgresql+psycopg2://postgres:downing@localhost:5432/', echo=True)
 Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+
+# create a Session
+session = Session()
+
+hiphop = Genre(name="hip hop")
+session.add(hiphop)
+
+kanye = Artist(artist_picture_link="url", name="Kanye", spotify_id="spotify:kanye", popularity=100)
+kanye.genres += [hiphop]
+session.add(kanye)
+
+session.commit()
+
