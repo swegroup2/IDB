fb9d90d2f61dc408353a08125991ee6a6671e9c2
sarah <swang622@gmail.com>
2017-10-22 20:12:13 -0500

    changed schema

--- API/schema.py
+++ API/schema.py
@@ -8,27 +8,27 @@ Base = declarative_base()
 DColumn = partial(Column, nullable=False)
 
 articles_artists = Table('articles_artists', Base.metadata,
-			DColumn('article_id', String, ForeignKey('articles.article_id'), primary_key=True),
-			DColumn('artist_id', String, ForeignKey('artists.artist_id'), primary_key=True))
+			DColumn('article_id', Integer, ForeignKey('articles.article_id'), primary_key=True),
+			DColumn('artist_id', Integer, ForeignKey('artists.artist_id'), primary_key=True))
 
 articles_albums = Table('articles_albums', Base.metadata,
-			DColumn('article_id', String, ForeignKey('articles.article_id'), primary_key=True),
-			DColumn('album_id', String, ForeignKey('albums.album_id'), primary_key=True))
+			DColumn('article_id', Integer, ForeignKey('articles.article_id'), primary_key=True),
+			DColumn('album_id', Integer, ForeignKey('albums.album_id'), primary_key=True))
 
 genres_artists = Table('genres_artists', Base.metadata,
-			DColumn('genre_id', String, ForeignKey('genres.genre_id'), primary_key=True),
-			DColumn('artist_id', String, ForeignKey('artists.artist_id'), primary_key=True))
+			DColumn('genre_id', Integer, ForeignKey('genres.genre_id'), primary_key=True),
+			DColumn('artist_id', Integer, ForeignKey('artists.artist_id'), primary_key=True))
 
 cities_artists = Table('cities_artists', Base.metadata,
-			DColumn('city_id', String, ForeignKey('cities.city_id'), primary_key=True),
-			DColumn('artist_id', String, ForeignKey('artists.artist_id'), primary_key=True))
+			DColumn('city_id', Integer, ForeignKey('cities.city_id'), primary_key=True),
+			DColumn('artist_id', Integer, ForeignKey('artists.artist_id'), primary_key=True))
 
 class Artist(Base):
 	__tablename__ = "artists"
 
-	artist_id = DColumn(String, primary_key=True)
+	artist_id = DColumn(Integer, primary_key=True)
 	name = DColumn(String)
-	spotify_link = DColumn(String, unique=True)
+	spotify_id = DColumn(String, unique=True)
 	artist_picture_link = DColumn(String, unique=True)
 	popularity = DColumn(Integer)
 	articles = relationship(
@@ -47,12 +47,12 @@ class Artist(Base):
 class Album(Base):
 	__tablename__ = "albums"
 
-	album_id = DColumn(String, primary_key=True)
+	album_id = DColumn(Integer, primary_key=True)
 	name = DColumn(String)
-	spotify_link = DColumn(String, unique=True)
+	spotify_id = DColumn(String, unique=True)
 	release_date = DColumn(Date)
 	album_picture_link = DColumn(String, unique=True)
-	artist_id = DColumn(String, ForeignKey("artists.artist_id"))
+	artist_id = DColumn(Integer, ForeignKey("artists.artist_id"))
 	articles = relationship(
 		'Article',
 		secondary=articles_albums,
@@ -73,7 +73,7 @@ class Track(Base):
 
 	name = DColumn(String, primary_key=True)
 	track_number = DColumn(Integer)
-	spotify_link = DColumn(String, unique=True)
+	spotify_id = DColumn(String, unique=True)
 	album_id = DColumn(Integer, ForeignKey("albums.album_id"), primary_key=True)
 
 class Region(Base):
@@ -85,7 +85,7 @@ class Region(Base):
 class Article(Base):
 	__tablename__ = "articles"
 
-	article_id = DColumn(String, primary_key=True)
+	article_id = DColumn(Integer, primary_key=True)
 	title = DColumn(String)
 	media_link = DColumn(String, unique=True)
 	date = DColumn(Date)
@@ -102,7 +102,7 @@ class Article(Base):
 class City(Base):
 	__tablename__ = "cities"
 
-	city_id = DColumn(String, primary_key=True)
+	city_id = DColumn(Integer, primary_key=True)
 	name = DColumn(String)
 	population = DColumn(Integer)
 	city_picture_link = DColumn(String, unique=True)
