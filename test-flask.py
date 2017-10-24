f7c50e56c1fcef69a41e501543f59191acbd5661
William Xu <williamxu@utexas.edu>
2017-10-23 23:01:09 -0500

    Fixed unit test suite and tested top_artists function

--- test-flask.py
+++ test-flask.py
@@ -1,13 +1,19 @@
 import unittest
 import os
 from database.schema import *
+from flask import jsonify
+import json
 
 class TestEndpoints(unittest.TestCase):
-	def setUp():
+	def setUp(self):
 		# TODO: connect to test db? Maybe create separate testcase.
 		os.environ["SQLALCHEMY_DATABASE_URI"] = "sqlite:///:memory:"
-		import main as app
-		self.app = app
+		import main as main_app
+		self.app = main_app #DB and app (should be just DB)
+		self.client = main_app.app.test_client()
+
+
+		Base.metadata.create_all(self.app.db.engine)
 
 		# Create some sample data
 		hiphop = Genre(name="Hip Hop")
@@ -16,17 +22,20 @@ class TestEndpoints(unittest.TestCase):
 		kanye = Artist(name="Kanye", spotify_id="spotify:kanye", artist_picture_link="//kanye", popularity=100)
 		jayz = Artist(name="Jay-Z", spotify_id="spotify:jayz", artist_picture_link="//jayz", popularity=90)
 		kanye.genres += [hiphop]
-		jays.genres += [hiphop]
+		jayz.genres += [hiphop]
 		artists = [kanye, jayz]
 
 		items = [*artists, *genres]
-		self.app.db.session.add(items)
+
+		for item in items:
+			self.app.db.session.add(item)
+		self.app.db.session.commit()
 
 	def test_artists_top(self):
 		# TODO: improve
-		res = self.app.show_artists_top(5)
-		self.assertTrue(type(res) == str)
-		self.assertTrue(len(res) > 0)
+		res = self.client.get("/api/artists/top")
+		json_Obj = json.loads(res.data)
+		print(json.dumps(json_Obj,indent=4,sort_keys=True))
 
 if __name__ == '__main__':
 	unittest.main()
