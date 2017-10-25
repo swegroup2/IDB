20c234f8355d6217671f0502093bda1efba794b2
William Xu <williamxu@utexas.edu>
2017-10-24 22:22:01 -0500

    Implemented API for albums (get_all_albums and get_specific_albums)

--- main.py
+++ main.py
@@ -27,10 +27,24 @@ def get_all_artists():
 	matches = db.session.query(Artist).all()
 	return sql_json(Artist, *matches)
 
-@app.route('/api/artists/<int:id>')
+@app.route('/api/artists/<int:a_id>')
 def get_specific_artist(a_id=1):
 	artist_id = a_id
 	matches = db.session.query(Artist).filter_by(artist_id=artist_id).first()
+	return sql_json(Artist, *matches)
+
+@app.route('/api/albums')
+def get_all_albums():
+	matches = db.session.query(Album).all()
+	return sql_json(Album, *matches)
+
+@app.route('/api/albums/<int:id>')
+def get_specific_album(a_id=1):
+	matches = db.session.query(Album).get(a_id)
+	return sql_json(Album, *matches)
+	
+
+
 
 @app.route('/api/artists/top/<int:num>')
 @app.route('/api/artists/top')
