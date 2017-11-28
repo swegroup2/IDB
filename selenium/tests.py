36021670fe4bd54a762b579c0830e758b66e9f64
chrcarroll <cjcarroll@utexas.edu>
2017-11-28 15:24:48 -0600

    test homepage

--- selenium/tests.py
+++ selenium/tests.py
@@ -1,4 +1,5 @@
 import unittest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
@@ -7,15 +8,29 @@ class PythonOrgSearch(unittest.TestCase):
     def setUp(self):
         self.driver = webdriver.Chrome()
 
-    def test_search_in_python_org(self):
-        driver = self.driver
-        driver.get("http://www.python.org")
-        self.assertIn("Python", driver.title)
-        elem = driver.find_element_by_name("q")
-        elem.send_keys("pycon")
-        elem.send_keys(Keys.RETURN)
-        assert "No results found." not in driver.page_source
+    # def test_home(self):
+    #     driver = self.driver
+    #     driver.get("https://poupon.me")
+    #     self.assertIn("Grey Poupon", driver.title)
 
+    #     #check for navbar
+    #     driver.find_element_by_xpath("//*[@id=\"root\"]/div/nav")
+
+    #     #check for header
+    #     header = driver.find_element_by_xpath("//*[@id=\"root\"]/div/div/div/div/div/h1")
+    #     self.assertIn("Welcome", header.text)
+
+    #     #check for video
+    #     driver.find_element_by_xpath("//*[@id=\"root\"]/div/div/div/div/div/iframe")
+
+    def test_about(self):
+        driver = self.driver
+        driver.get("https://poupon.me")
+        button = driver.find_element_by_xpath("//*[@id=\"navbarSupportedContent\"]/ul/li[2]/a/div")
+        button.click()
+        self.assertEqual(driver.current_url, "https://poupon.me/about")
+        cards = driver.find_elements_by_xpath("//*[@id=\"root\"]/div/div/div/div/div")
+        self.assertEqual(len(cards), 6)
 
     def tearDown(self):
         self.driver.close()
