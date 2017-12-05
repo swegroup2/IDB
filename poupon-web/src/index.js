b06625a10921441e03ed981cc7fa1ca7af6b6163
Logan Zartman <logan.zartman@utexas.edu>
2017-12-05 00:06:15 -0600

    Created custom bootstrap package

--- poupon-web/src/index.js
+++ poupon-web/src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+require('poupon-bootstrap');
 
 ReactDOM.render(<App/>, document.getElementById('root'));
